4c87435b454a278f6d533e34f17b85585d96249e
Miss Terry Davis <tdavis_909@hotmail.com>
2026-07-06 15:25:08 +0100

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -57,12 +57,13 @@ def validate_data(values):
 
 def update_worksheet(data, worksheet):
     """
-    Update the specified worksheet, add new row with the list data provided
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
     """
     print(f"Updating {worksheet} worksheet...\n")
-    target_worksheet = SHEET.worksheet(worksheet)
-    target_worksheet.append_row(data)
-    print(f"{worksheet} worksheet updated successfully.\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -76,7 +77,6 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
 
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
@@ -102,6 +102,22 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -111,9 +127,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
-sales_columns = get_last_5_entries_sales()
